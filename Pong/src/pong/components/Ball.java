d4d8ac66687a2aebb806cb7b3c29bff4e2c8e6f0
lusterlink <lusterlink@ef9bbc28-cd15-7abf-f4c1-1de74909514b>
2010-05-28 04:45:59 +0000

    Addded some basic headers.

---
+++ Pong/src/pong/components/Ball.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pong.components;
+
+/** A single ball in the game.  Knows about its point and is responsible for
+ *  its own image.
+ *
+ * @author CJ
+ */
+public class Ball {
+
+}
---
+++ Pong/src/pong/util/Point3D.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pong.util;
+
+/** A representation of a 3D point in space.
+ *
+ * @author CJ
+ */
+public class Point3D {
+
+}
