383b5f159c79993ae17ed2f0411b4bb963ea1784
lusterlink <lusterlink@ef9bbc28-cd15-7abf-f4c1-1de74909514b>
2010-05-28 04:42:50 +0000

    Addded some basic headers.

---
+++ trunk/Pong/src/pong/components/Component.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pong.components;
+
+/**Basic Component that everything in the game must extend.
+ *
+ * @author CJ
+ */
+public class Component {
+
+}
---
+++ trunk/Pong/src/pong/constants/Constants.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pong.constants;
+
+/**Stores the Constants for the 3D Pong game.
+ *
+ * @author CJ
+ */
+public class Constants {
+
+    //Uninstanciable
+    private Constants(){}
+
+
+
+}
