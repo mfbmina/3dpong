016bbfc78efd2a3cde2624b472bb0bb6cefe61df
thnbgr@gmail.com <thnbgr@gmail.com@ef9bbc28-cd15-7abf-f4c1-1de74909514b>
2010-05-30 05:07:38 +0000

    points int declaration

---
+++ trunk/Pong/src/pong/components/Paddle.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pong.components;
+
+/**
+ *
+ * @author eric
+ */
+public class Paddle {
+
+    public Paddle() {
+
+    }
+
+    public void draw() {
+
+    }
+}
