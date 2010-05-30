e67ec7d250210d04377195cedc3d9e27b8f7bb23
thnbgr <thnbgr@gmail.com>
2010-05-30 21:24:08 +0000

    GameEngine stuff

--- Pong/src/pong/frontEnd/GameEngine.java
+++ Pong/src/pong/frontEnd/GameEngine.java
@@ -5,34 +5,51 @@
 
 package pong.frontEnd;
 
+import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import javax.swing.JPanel;
 import pong.constants.Constants;
+import pong.components.*;
 
 /** Main Game Engine that controls majority of the interactions between classes.
  *
  * @author CJ
  */
-public class GameEngine implements MouseListener, MouseMotionListener {
+public class GameEngine extends JPanel implements MouseListener, MouseMotionListener {
 
     public GameEngine() {
 
+       addMouseListener(this);
+       addMouseMotionListener(this);
+
+       initGame();
     }
 
     public void run() {
         try {
             Thread.sleep(Constants.DELAY);
-            
+
         } catch (InterruptedException ex) {}
+    }
+
+    public void initGame() {
+
+    }
+
+
+    public void PaintComponent(Graphics g) {
+
     }
 
 
 
 
 
-
-
+/*
+ * Mouse Methods implemented from MouseListener and MouseMotionListener
+ */
     
     public void mouseClicked(MouseEvent e) {
         throw new UnsupportedOperationException("Not supported yet.");
