1a685eafa484fdb8311821a55d0472d8869f2fc8
lusterlink <lusterlink@ef9bbc28-cd15-7abf-f4c1-1de74909514b>
2010-07-20 05:01:01 +0000

    Ball can now bounce around, need to implement the 3D shift.

--- trunk/Pong/src/pong/constants/Constants.java
+++ trunk/Pong/src/pong/constants/Constants.java
@@ -17,6 +17,11 @@ public class Constants {
     public static int SCREEN_HEIGHT = 400;
     public static int SCREEN_DEPTH = 800;
 
+    //Plane ID
+    public static int PLANE_Z = 0;
+    public static int PLANE_X = 1;
+    public static int PLANE_Y = 2;
+
     public static int MAX_BALL_RADIUS = 50;
     
     private Constants(){}
